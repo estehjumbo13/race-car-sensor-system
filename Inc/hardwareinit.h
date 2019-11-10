978cba093ff1aaede9c0a80003099d74dcfccde9
galang-room <galangperdana66@gmail.com>
2019-11-10 20:01:18 +0700

    first commit

---
+++ Inc/hardwareinit.h
@@ -0,0 +1,15 @@
+
+
+#ifndef HARDWAREINIT_H_
+#define HARDWAREINIT_H_
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_TIM2_Init(void);
+void MX_USART1_UART_Init(void);
+void MX_I2C2_Init(void);
+void MX_TIM4_Init(uint32_t Periode);
+void System_Init(void);
+
+#endif /* HARDWAREINIT_H_ */
